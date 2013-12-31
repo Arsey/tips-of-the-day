ca14bd4dd17d8488c2b12854a67252d4022012f4
MatsMaker <matsitskyss@gmail.com>
2013-12-31 10:40:45 +0200

    Revert "revie added new category"
    This reverts commit 387dada01f3496f604a7b3a2581b103776d46269.

--- config/routes.js
+++ config/routes.js
@@ -9,8 +9,4 @@ module.exports = function(app) {
     //Home route
     var index = require('../app/controllers/index');
     app.get('/', index.render);
-
-    // Category route
-    var category = require('../app/controllers/category');
-    app.get('/category', category.all);
 };
