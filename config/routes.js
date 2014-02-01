d8593f643c5cc7c3c8fb07b8f04f64f1e7d72e58
matsmaker <matsitskyss@gmail.com>
2014-02-01 11:10:20 +0200

    small fix for work addLikes

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
     app.get('/tips', tips.all);
 
     app.post('/tips', tips.create);
-    app.post('/tips/addlike/:tipId', tips.addlike);
+    app.post('/tips/:tipId', tips.addlike);
 
     //Edit tip
     app.get('/tips/getonetip/:tipId', tips.getOneTip);
